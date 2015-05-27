507cf3390acb65db67da285a2e804ebb1abfaffb
erik van widenfelt <ew2789@gmail.com>
2015-05-27 10:04:56 +0200

    remove MAX_SUBJECTS exception

--- edc_registration/models/__init__.py
+++ edc_registration/models/__init__.py
@@ -5,9 +5,3 @@ from .base_registered_subject_model import BaseRegisteredSubjectModel
 from .base_registration_model import BaseRegistrationModel
 from .registered_subject import RegisteredSubject
 from .subject_identifier_audit_trail import SubjectIdentifierAuditTrail
-
-
-if 'MAX_SUBJECTS' not in dir(settings):
-    raise ImproperlyConfigured(
-        'Missing settings attribute MAX_SUBJECTS. Please add settings attribute '
-        'MAX_SUBJECTS = to your settings file.')

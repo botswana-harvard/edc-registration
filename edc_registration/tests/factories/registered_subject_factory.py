54da56e485625c6f74c3de1e39e349170923b49e
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-08-25 13:13:52 +0200

    removing strore samples in registered subject factory

--- edc_registration/tests/factories/registered_subject_factory.py
+++ edc_registration/tests/factories/registered_subject_factory.py
@@ -11,7 +11,6 @@ class RegisteredSubjectFactory(BaseSubjectFactory):
 
     identity = factory.Sequence(lambda n: '11111111{0}'.format(n))
     identity_type = 'OMANG'
-    may_store_samples = 'Yes'
 
     class Meta:
         abstract = False

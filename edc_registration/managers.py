beddc1ec5e422c3823fc348489b4a06825e56ee4
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2016-12-01 10:09:33 +0200

    removed manager methods

--- edc_registration/managers.py
+++ edc_registration/managers.py
@@ -5,3 +5,12 @@ class RegisteredSubjectManager(models.Manager):
 
     def get_by_natural_key(self, subject_identifier_as_pk):
         return self.get(subject_identifier_as_pk=subject_identifier_as_pk)
+
+    def get_for_subject_identifier(self, subject_identifier):
+        """Returns a queryset for the given subject_identifier."""
+        options = {'subject_identifier': subject_identifier}
+        return self.filter(**options)
+
+    def get_for_visit(self, visit):
+        options = {'subject_identifier': visit.subject_identifier}
+        return self.get(**options)

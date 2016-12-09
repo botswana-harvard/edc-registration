4c424c69a96db6f18d35236c24eff56949253010
erikvw <ew2789@gmail.com>
2016-12-09 18:03:23 +0200

    remove transaction atomic

--- edc_registration/signals.py
+++ edc_registration/signals.py
@@ -1,15 +1,14 @@
-from django.db import transaction
+# from django.db import transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
 @receiver(post_save, weak=False, dispatch_uid="update_registered_subject_from_consent_on_post_save")
 def update_registered_subject_from_consent_on_post_save(sender, instance, raw, created, using, **kwargs):
-    """Updates RegisteredSubject from the consent or model using UpdatesOrCreatesRegistrationModelMixin."""
+    """Updates RegisteredSubject from models using UpdatesOrCreatesRegistrationModelMixin."""
     if not raw and not kwargs.get('update_fields'):
         try:
-            with transaction.atomic():
-                instance.registration_update_or_create()
+            instance.registration_update_or_create()
         except AttributeError as e:
             if 'registration_update_or_create' not in str(e):
                 raise AttributeError(str(e))

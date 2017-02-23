e093acf8c2cbe419fbf7ca40bade12566b578117
erikvw <ew2789@gmail.com>
2017-02-23 22:45:20 +0200

    add app_name

--- edc_registration/urls.py
+++ edc_registration/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from .admin_site import edc_registration_admin
 
+app_name = 'edc_registration'
 
 urlpatterns = [
     url(r'^admin/', edc_registration_admin.urls),

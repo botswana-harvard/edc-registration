84fa8564031c7138225d235b4353c852ee0a7956
erikvw <ew2789@gmail.com>
2017-07-27 14:31:22 +0200

    redirect home url

--- edc_registration/urls.py
+++ edc_registration/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
 from .admin_site import edc_registration_admin
+from django.views.generic.base import RedirectView
 
 app_name = 'edc_registration'
 
 urlpatterns = [
     url(r'^admin/', edc_registration_admin.urls),
+    url(r'', RedirectView.as_view(url='/'), name='home_url'),
 ]

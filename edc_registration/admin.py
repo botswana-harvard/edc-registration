20fb3e260d365b9ff1da6c2162bc74d1c5a2564b
erikvw <ew2789@gmail.com>
2017-08-15 14:02:03 +0200

    minor

--- edc_registration/admin.py
+++ edc_registration/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-
 from .admin_mixins import RegisteredSubjectModelAdminMixin
 from .admin_site import edc_registration_admin
 from .models import RegisteredSubject

fc2fe626849dc4fcb661e5dedb771ffc1cfae235
erikvw <ew2789@gmail.com>
2017-12-06 04:11:22 +0200

    remove duplicate url

--- edc_registration/urls.py
+++ edc_registration/urls.py
@@ -6,7 +6,6 @@ from .admin_site import edc_registration_admin
 app_name = 'edc_registration'
 
 urlpatterns = [
-    path('admin/edc_registration/', edc_registration_admin.urls),
     path('admin/', edc_registration_admin.urls),
     path('', RedirectView.as_view(url='admin/edc_registration/'), name='home_url'),
 ]

a35b015358ef92fee530ac1d2f5d3e7aa1302f22
erikvw <ew2789@gmail.com>
2017-12-28 20:17:18 +0200

    add migration

--- edc_registration/settings.py
+++ edc_registration/settings.py
@@ -16,7 +16,10 @@ import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 APP_NAME = 'edc_registration'
+ETC_DIR = os.path.join(BASE_DIR, 'etc')
 SITE_ID = 40
+# AUTO_CREATE_KEYS = True
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -138,6 +141,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 GIT_DIR = BASE_DIR
-KEY_PATH = os.path.join(BASE_DIR, 'crypto_fields')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
